3bc51a6298f3e9548308d6bf39fb68ddbd2c0fc9
Alan Cano <alancorreonuevo5@gmail.com>
2024-04-30 15:14:09 -0500

    changes

---
+++ Programs/TotalCoder_Course/modules/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Stransyyy/Learning-Journey-Programs/Programs/TotalCoder_Course/modules/calculate"
+)
+
+func main() {
+	res := calculate.Add(1, 2)
+
+	fmt.Println(res)
+}
