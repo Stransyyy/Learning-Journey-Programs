11c67bc4bd915e5f09fe3635a1413c6bd85deda5
Alan Cano <alancorreonuevo5@gmail.com>
2024-04-30 16:02:36 -0500

    changes on channels

--- Programs/TotalCoder_Course/goRoutines/main.go
+++ Programs/TotalCoder_Course/goRoutines/main.go
@@ -4,25 +4,31 @@ import (
 	"fmt"
 )
 
-func eatCookie(messageCh chan string) {
-	result := <-messageCh
-
-	fmt.Println(result)
+func eatCookies(messageCh chan string) {
+	for message := range messageCh {
+		fmt.Println("reading from channel", message)
+	}
 }
 
 // A channel always blocks if its full
 // A unbuffered channel is always full
 
 func main() {
-	messageCh := make(chan string) // This is a unbuffered channel; how to make it buffered?
+	messageCh := make(chan string, 128) // This is a unbuffered channel; how to make it buffered?
 	//make(chan string, 1) <-- specify the size of the buffer
 	//1. Write to a channel
 	//2. Read to a channel
 
 	// bufferedCh := make(chan string, 1) // buffered channel
 
-	go eatCookie(messageCh)
+	for i := 0; i < 100; i++ {
+		messageCh <- fmt.Sprintf("hello %d\n", i)
+	}
 
-	messageCh <- "Hello!" // blocking here
+	close(messageCh)
+
+	eatCookies(messageCh)
+
+	// messageCh <- "Hello!" // blocking here
 
 }
