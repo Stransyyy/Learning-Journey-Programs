afa093af0691cff51473b80b08c7251dee79d767
Alan Cano <alancorreonuevo5@gmail.com>
2024-05-07 14:26:09 -0500

    changes

--- Programs/cli/main.go
+++ Programs/cli/main.go
@@ -7,10 +7,36 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/fatih/color"
 )
 
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Region  string `json:"region"`
+		Country string `json:"country"`
+		Time    string `json:"localtime"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		TempF     float64 `json:"temp_f"`
+		Humidity  int     `json:"humidity"`
+		UV        float64 `json:"uv"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+}
+
 func main() {
-	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=e60f2ddf09bc4b3091c140905240705&q=Tupelo&aqi=no")
+	q := "Tupelo"
+
+	if len(os.Args) >= 2 {
+		q = os.Args[1]
+	}
+
+	res, err := http.Get("http://api.weatherapi.com/v1/current.json?key=e60f2ddf09bc4b3091c140905240705&q=" + q + "&aqi=no")
 	if err != nil {
 		fmt.Println("HTTP request failed: ", err)
 		return
@@ -32,32 +58,66 @@ func main() {
 		return
 	}
 
-	// file name for our json file
-	fileName := "weather.json"
-
-	// we check if it exist already
-	if fi, err := os.Stat(fileName); err == nil {
-		size := fi.Size()
-		fmt.Printf("file already exists, size: %v bytes\n", size)
-		if err := os.WriteFile(fileName, body, 0666); err != nil {
-			fmt.Println("Failed to write to file: ", err)
-			return
-		}
-		fmt.Println("File created successfully!")
-	} else if err != nil {
-		fmt.Println("Error checking for file")
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		fmt.Println("Failed to unmarshal JSON: ", err)
+		return
 	}
 
-	// Pretty print API response to terminal
-	// prettyBody, err := prettyJson(body)
-	// if err != nil {
-	// 	fmt.Println("Failed to pretty print JSON: ", err)
-	// 	return
-	// }
-	// fmt.Printf("%s\n", prettyBody)
+	location, current := weather.Location, weather.Current
 
-	// since we will make a cli, we won't need this pretty print
+	// Color settings for temperature
+	tempCColor := color.New(color.FgCyan) // Default color
+	if current.TempC >= 30 {
+		tempCColor = color.New(color.FgRed)
+	} else if current.TempC <= 0 {
+		tempCColor = color.New(color.FgBlue)
+	}
+	tempCString := tempCColor.Sprintf("%.0fC°", current.TempC)
 
+	tempFColor := color.New(color.FgCyan) // Default color
+	if current.TempF >= 86 {
+		tempFColor = color.New(color.FgRed)
+	} else if current.TempF <= 32 {
+		tempFColor = color.New(color.FgBlue)
+	}
+	tempFString := tempFColor.Sprintf("%.0fF°", current.TempF)
+
+	// Color settings for humidity
+	humidityColor := color.New(color.FgYellow) // Default color
+	if current.Humidity >= 80 {
+		humidityColor = color.New(color.FgRed)
+	} else if current.Humidity <= 20 {
+		humidityColor = color.New(color.FgBlue)
+	}
+	humidityString := humidityColor.Sprintf("%v%%", current.Humidity)
+
+	// Color settings for UV
+	uvColor := color.New(color.FgHiWhite)
+	switch {
+	case current.UV < 3:
+		uvColor = color.New(color.FgGreen)
+	case current.UV < 6:
+		uvColor = color.New(color.FgYellow)
+	case current.UV >= 6 && current.UV < 8:
+		uvColor = color.New(color.FgRed)
+	case current.UV >= 8 && current.UV < 11:
+		uvColor = color.New(color.FgMagenta)
+	case current.UV >= 11:
+		uvColor = color.New(color.FgHiBlue)
+	}
+
+	uvStr := uvColor.Sprintf("%.0f", current.UV)
+
+	// Constructing the message with color formatting
+	message := fmt.Sprintf("Location: %s, %s, %s\nWeather: %s, %s\nCondition of the day: %s\nExtra: Humidity: %s, UV:",
+		location.Name, location.Region, location.Country,
+		tempCString, tempFString,
+		current.Condition.Text,
+		humidityString)
+
+	fmt.Print(message + uvStr + " Time: " + location.Time + "\n")
 }
 
 func prettyJson(body []byte) ([]byte, error) {
--- go.mod
+++ go.mod
@@ -9,8 +9,14 @@ require (
 )
 
 require (
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
+
+require (
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
-	github.com/google/uuid v1.5.0
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 )
